bf2b239eb2978dacec525b47f19bdc6fc95eeb7e
María Torrente <caritotorrente@gmail.com>
2020-12-01 08:35:59 -0500

    setting endpoint to get a list by ID

--- src/app.js
+++ src/app.js
@@ -67,6 +67,18 @@ app.get("/card/:id", (req, res) => {
   }
 
   res.json(card);
+  app.get("/list/:id", (req, res) => {
+    const { id } = req.params;
+    const list = lists.find((li) => li.id == id);
+
+    // make sure we found a list
+    if (!list) {
+      logger.error(`List with id ${id} not found.`);
+      return res.status(404).send("List Not Found");
+    }
+
+    res.json(list);
+  });
 });
 app.use(function errorHandler(error, req, res, next) {
   let response;
