8131f3079434d39ccbcc497c57c8162b4658717c
María Torrente <caritotorrente@gmail.com>
2020-11-30 22:57:59 -0500

    get endpoints

---
+++ src/app.js
@@ -0,0 +1,84 @@
+require("dotenv").config();
+const winston = require("winston");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+
+const app = express();
+const cards = [
+  {
+    id: 1,
+    title: "Task One",
+    content: "This is card one",
+  },
+];
+const lists = [
+  {
+    id: 1,
+    header: "List One",
+    cardIds: [1],
+  },
+];
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+// set up winston
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "info.log" })],
+});
+
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
+});
+app.get("/card", (req, res) => {
+  res.json(cards);
+});
+
+app.get("/list", (req, res) => {
+  res.json(lists);
+});
+app.get("/card/:id", (req, res) => {
+  const { id } = req.params;
+  const card = cards.find((c) => c.id == id);
+
+  // make sure we found a card
+  if (!card) {
+    logger.error(`Card with id ${id} not found.`);
+    return res.status(404).send("Card Not Found");
+  }
+
+  res.json(card);
+});
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+app.use(cors());
+
+module.exports = app;
